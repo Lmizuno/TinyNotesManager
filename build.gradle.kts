c71a050956b3afef3d226bf49b39e5b2ae00399c
Luis <luismizuno1299@gmail.com>
2025-04-05 15:37:52 -0400

    Update KPS to compatible version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.devtools.ksp") version "2.0.10-1.0.24" apply false
     id("org.jetbrains.kotlin.android") version "2.0.10" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.3" apply false
